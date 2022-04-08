75697c82630f1c0ceb19ee0450c01cc4a4312b57
Nathcat <supernath126@gmail.com>
2022-04-08 13:29:57 +0100

    `CreateVertexBuffer()` function complete, more tasks required for creating the transformation matrix constant buffer

--- MeshRenderer.h
+++ MeshRenderer.h
@@ -41,8 +41,56 @@ public:
 	/// Required tasks:
 	///  - Shader input struct/class
 	/// </summary>
+	/// <param name="dev">Pointer to DirectX device</param>
+	/// <param name="devcon">Pointer to DirectX device context</param>
 	/// <returns>A pointer to the vertex buffer</returns>
-	ID3D11Buffer* CreateVertexBuffer() {
+	ID3D11Buffer* CreateVertexBuffer(ID3D11Device* dev, ID3D11DeviceContext* devcon) {
+		ID3D11Buffer* buffer;
 
+		D3D11_BUFFER_DESC bd;
+		ZeroMemory(&bd, sizeof(D3D11_BUFFER_DESC));                                           // Clear the buffer description struct
+
+		bd.Usage = D3D11_USAGE_DYNAMIC;                                                       // Access from CPU and GPU
+		bd.ByteWidth = material->GetSizeOfElementsArray() * mesh->numberOfVertices;           // Set the byte width of the buffer
+		bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;                                              // Set as vertex buffer
+		bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;                                           // Allow the CPU to write to the buffer
+
+		dev->CreateBuffer(&bd, NULL, &buffer);                                                // Create the buffer
+
+		D3D11_MAPPED_SUBRESOURCE ms;                                                          // Mapped buffer
+		devcon->Map(buffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);                        // Map the buffer, preventing the GPU from using it while vertices are copied to it
+		memcpy(ms.pData, mesh->pVertices, material->GetSizeOfElementsArray() * mesh->numberOfVertices);  // Copy the vertex data to the buffer
+		devcon->Unmap(buffer, NULL);                                                          // Unmap the buffer
+
+		return buffer;
+	}
+
+	/// <summary>
+	/// Creates a buffer of transform matrices used to transform vertices onto the screen.
+	/// </summary>
+	/// <param name="dev">Pointer to the DirectX device.</param>
+	/// <param name="devcon">Pointer to the DirectX device context.</param>
+	/// <returns>Pointer to a buffer object.</returns>
+	ID3D11Buffer* CreateTransformBuffer(ID3D11Device* dev, ID3D11DeviceContext* devcon) {
+		ID3D11Buffer* buffer;
+
+		D3D11_BUFFER_DESC bd;
+		ZeroMemory(&bd, sizeof(D3D11_BUFFER_DESC));                                           // Clear the buffer description struct
+
+		bd.Usage = D3D11_USAGE_DYNAMIC;                                                       // Access from CPU and GPU
+		bd.ByteWidth = material->GetSizeOfElementsArray() * mesh->numberOfVertices;           // Set the byte width of the buffer
+		bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;                                            // Set as vertex buffer
+		bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+		dev->CreateBuffer(&bd, NULL, &buffer);
+
+		// TODO:
+		//  - Get transform matrices and copy to buffer
+		//
+		// Required tasks:
+		//  - Transform.CreateTransformMatrix
+		//  - Camera.CreateViewMatrix
+		//  - Camera.CreateProjectionMatrix
+		//  - Camera component
 	}
 };
